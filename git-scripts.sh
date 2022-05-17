e5bb79a64b3dfd0e69e01219089735d1799e1cbf
Sam <samuele@zanca.dev>
2022-05-17 13:51:51 -0400

    Added git pub for publishing new branch

--- git-scripts.sh
+++ git-scripts.sh
@@ -10,6 +10,7 @@ git config --global alias.initial "!sh $PWD/bin/initial.sh";
 git config --global alias.master "!sh $PWD/bin/master.sh";
 git config --global alias.new "!sh $PWD/bin/new.sh";
 git config --global alias.pr "!node $PWD/bin/pr.js";
+git config --global alias.pub "!sh $PWD/bin/publish.sh";
 # git config --global alias.pr "!node $PWD/bin/sync.js"; # WIP
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
